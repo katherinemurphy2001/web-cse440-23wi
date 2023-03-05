2aff0d1cbad8d289f68b8e94e81494e5bcdc32e2
maxo20 <126521624+maxo20@users.noreply.github.com>
2023-03-04 21:52:33 -0800

    Unset the entire alt tag if not specified
    Per:
    https://github.com/uwcse440/web-cse440-23wi/pull/34#issuecomment-1454918260

--- src/components/project/ProjectImagesBlock.tsx
+++ src/components/project/ProjectImagesBlock.tsx
@@ -62,7 +62,7 @@ export const ProjectImagesBlock: FunctionComponent<ProjectImagesBlockProps> = (p
                                                         maxHeight: (props.maxHeight === undefined) ? "400px" : props.maxHeight,
                                                         maxWidth: "100%"
                                                     }}
-                                                    alt={projectImage.alt ? `${projectImage.alt}` : 'Missing alt text.'}
+                                                    alt={projectImage.alt ? `${projectImage.alt}` : undefined}
                                                 />
                                             </Paper>
                                             {
